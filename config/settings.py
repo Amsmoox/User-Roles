dfd1606dbe813561146db96152cc34e941dbf662
“Amsmoox” <amsmoox@gmail.com>
2025-05-09 23:59:16 +0100

    Update DATABASES configuration in settings.py to use PostgreSQL and set AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(default="sqlite:///db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME', default=''),
+        'USER': env('DB_USER', default=''),
+        'PASSWORD': env('DB_PASSWORD', default=''),
+        'HOST': env('DB_HOST', default=''),
+        'PORT': env('DB_PORT', default=''),
+    }
 }
 
 
